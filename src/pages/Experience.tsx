d9579aadd7e14f587f0ba6ccccb8c9ba934a27d2
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2026-07-06 17:26:39 -0700

    exp base

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -13,9 +13,9 @@ export default function Experience({ pageRef }: Prompts) {
                 <span className="text-4xl opacity-10 font-bold">{'< >'}</span>
             </div>
 
-            <ul className="flex flex-col w-full gap-8">
-                <li className="relative flex gap-8 justify-between w-full">
-                    <div className="flex flex-col flex-1 max-w-1/2 h-fit gap-4 px-4 py-2 bg-(--neutral) rounded-xl">
+            <ul className="flex flex-col w-full mb-8">
+                <li className="relative flex gap-8 justify-between w-full h-fit">
+                    <div className="flex flex-col flex-1 max-w-1/2 h-fit gap-4 px-4 py-2 bg-(--neutral) rounded-xl my-8">
                         <span className="text-sm text-(--gray)">UABC - Departamento De Desarrollo de Software</span>
                         <div className="flex w-full justify-between">
                             <h1 className="text-4xl font-bold">Titulo</h1>
@@ -33,16 +33,18 @@ export default function Experience({ pageRef }: Prompts) {
                         </div>
                     </div>
 
-                    <div className="bg-white flex-1 max-w-px"></div>
-                    <div className="absolute top-0 right-1/2 -translate-x-1/2 bg-white size-1 rounded-full"></div>
+                    <div className="absolute top-0 right-1/2 bg-white w-1 h-full -translate-x-1/3"></div>
+                    <div className="absolute top-0 right-1/2  bg-white size-2 rounded-full"></div>
 
-                    <div className="h-fit max-w-1/2 gap-4 px-4 py-2 bg-(--neutral) grow-0 rounded-xl">
-                        <img src="/achivements/icpc/0.webp" alt="Prueba" className="object-cover rounded-lg" />
+                    <div className="flex flex-1 max-w-1/2 min-h-full gap-4 px-4 py-2 bg-(--neutral) rounded-xl overflow-hidden my-8">
+                        <div className="relative w-full max-h-full overflow-hidden bg-red-500 rounded-lg">
+                            <img src="/achivements/icpc/0.webp" alt="Prueba" className="absolute top-0 left-0 w-full h-full object-cover rounded-lg" />
+                        </div>
                     </div>
                 </li>
                 
-                <li className="relative flex flex-row-reverse gap-8 justify-between w-full ">
-                    <div className="flex flex-col flex-1 max-w-1/2 gap-4 px-4 py-2 bg-(--neutral)  rounded-xl">
+                <li className="relative flex flex-row-reverse gap-8 justify-between w-full h-fit">
+                    <div className="flex flex-col flex-1 max-w-1/2 h-fit gap-4 px-4 py-2 bg-(--neutral) rounded-xl my-8">
                         <span className="text-sm text-(--gray)">UABC - Departamento De Desarrollo de Software</span>
                         <div className="flex w-full justify-between">
                             <h1 className="text-4xl font-bold">Titulo</h1>
@@ -60,13 +62,46 @@ export default function Experience({ pageRef }: Prompts) {
                         </div>
                     </div>
 
-                    <div className="bg-white h-full w-1"></div>
-                    <div className="absolute top-0 right-1/2 -translate-x-1/2 bg-white h-full w-1 rounded-full"></div>
+                    <div className="absolute top-0 right-1/2 bg-white w-1 h-full -translate-x-1/3"></div>
+                    <div className="absolute top-0 right-1/2  bg-white size-2 rounded-full"></div>
 
-                    <div className="flex flex-col flex-1 max-w-1/2 gap-4 px-4 py-2 bg-(--neutral) rounded-xl">
-                        <img src="/achivements/icpc/0.webp" alt="Prueba" className="w-full h-full object-cover rounded-lg" />
+                    <div className="flex flex-1 max-w-1/2 min-h-full gap-4 px-4 py-2 bg-(--neutral) rounded-xl overflow-hidden my-8">
+                        <div className="relative w-full max-h-full overflow-hidden bg-red-500 rounded-lg">
+                            <img src="/achivements/icpc/0.webp" alt="Prueba" className="absolute top-0 left-0 w-full h-full object-cover rounded-lg" />
+                        </div>
                     </div>
                 </li>
+
+                <li className="relative flex gap-8 justify-between w-full h-fit">
+                    <div className="flex flex-col flex-1 max-w-1/2 h-fit gap-4 px-4 py-2 bg-(--neutral) rounded-xl my-8">
+                        <span className="text-sm text-(--gray)">UABC - Departamento De Desarrollo de Software</span>
+                        <div className="flex w-full justify-between">
+                            <h1 className="text-4xl font-bold">Titulo</h1>
+                            <span className="text-lg">Ago 2022 - Jun 2026</span>
+                        </div>
+                        <p className="text-base">
+                            Desarrollé y mantuve sistemas académicos utilizados dentro de la universidad.
+                            Contribuí al desarrollo y mejora de aplicaciones internas universitarias.
+                            Mantuve, optimicé y mejoré módulos backend para garantizar confiabilidad y rendimiento.
+                            Colaboré con equipos multidisciplinarios, aplicando mejores prácticas de desarrollo de software.
+                        </p>
+                        <div className="flex gap-4">
+                            <Button label="NestJs" icon={{ src: Icons.nestJs, alt: "NestJs" }} onClick={() => { }} />
+                            <Button label="NestJs" icon={{ src: Icons.nestJs, alt: "NestJs" }} onClick={() => { }} />
+                        </div>
+                    </div>
+
+                    <div className="absolute top-0 right-1/2 bg-white w-1 h-full -translate-x-1/3"></div>
+                    <div className="absolute top-0 right-1/2  bg-white size-2 rounded-full"></div>
+
+                    <div className="flex flex-1 max-w-1/2 min-h-full gap-4 px-4 py-2 bg-(--neutral) rounded-xl overflow-hidden my-8">
+                        <div className="relative w-full max-h-full overflow-hidden bg-red-500 rounded-lg">
+                            <img src="/achivements/icpc/0.webp" alt="Prueba" className="absolute top-0 left-0 w-full h-full object-cover rounded-lg" />
+                        </div>
+                    </div>
+                </li>
+                
+                
             </ul>
         </div>
     )
